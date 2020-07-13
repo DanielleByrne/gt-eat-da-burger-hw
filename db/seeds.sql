3ae109f16548f7c79544147d66d7332375096fa2
Danielle Byrne <DanielleByrne421@gmail.com>
2020-07-13 18:02:49 -0400

    started burgers database

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Avocado Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Pepperjack Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheddar Burger');
+INSERT INTO burgers (burger_name) VALUES ('Teriyaki Salmon Burger');
