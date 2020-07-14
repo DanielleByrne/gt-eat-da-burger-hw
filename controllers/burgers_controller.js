4ea8fed0e788d0365f948a6f3b9e22a8267be221
Danielle Byrne <DanielleByrne421@gmail.com>
2020-07-14 00:34:34 -0400

    burgers in db already not showing on page, but selectAll method is generating html in browser

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,9 +4,16 @@ const router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
 const cat = require("../models/burger.js");
+const burger = require("../models/burger.js");
 
-
-
-
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    let hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,12 @@
 const orm = require("../config/orm.js");
 
 const burger = {
-// will call the ORM functions using burger specific input 
-
-}
+  // will call the ORM functions using burger specific input
+  selectAll: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res);
+    });
+  },
+};
 
 module.exports = burger;
