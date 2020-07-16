ff411c19d7324067a5d13cf570aa08e928deed93
Danielle Byrne <DanielleByrne421@gmail.com>
2020-07-15 20:26:17 -0400

    removing password from connection folder

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Cheeseplease421!",
+    password: "null",
     database: "burgers_db",
   });
 }
